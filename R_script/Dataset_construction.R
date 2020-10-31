f8d899bb12fc2ffc0cff786d8da8acbdac0c3972
Thomas <escandethomas@gmail.com>
2020-10-31 23:41:31 +0100

    Setting up work directory and dashboard
    First page of the dashboard with all required informations

---
+++ R_script/Dataset_construction.R
@@ -0,0 +1,7 @@
+#Prepare V - Dem dataset to be in the dashboard
+
+# Set the working directory
+setwd("C:/Users/escan/Dropbox/Horizontal/Dashboard/Shiny/Dataset/CPD_V-Party_R_v1")
+
+#Opening dataset
+Origin_data <- readRDS("V-Dem-CPD-Party-V1.rds")
